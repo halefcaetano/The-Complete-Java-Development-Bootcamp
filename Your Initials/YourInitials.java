df62be1bee70e5079c59b50a4683f70325f9fd31
HalefCaetano <halef_caetano@hotmail.com>
2023-12-15 18:59:35 -0800

    Your Initials

---
+++ Your Initials/YourInitials.java
@@ -0,0 +1,12 @@
+public class YourInitials {
+    public static void main(String[] args) {
+        System.out.println(" HHH     C  ");
+        System.out.println("H   H   C C");
+        System.out.println("H      C   C");
+        System.out.println(" HHH   CCCC");
+        System.out.println("    H  C  C");
+        System.out.println("H   H  C  C");
+        System.out.println("  HHH  C  C");
+    }
+
+}
