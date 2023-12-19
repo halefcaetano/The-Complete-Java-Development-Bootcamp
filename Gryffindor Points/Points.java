d46102225a41048ee19a5bc50337c008bffa70bc
HalefCaetano <halef_caetano@hotmail.com>
2023-12-18 18:51:50 -0800

    Survey

---
+++ Gryffindor Points/Points.java
@@ -0,0 +1,22 @@
+public class Points {
+    public static void main(String[] args) {
+        int pointsG = 0;
+        System.out.println("Gryffindor loses 50 points");
+        pointsG -= 50;
+        System.out.println("Gryffindor loses 3 points");
+        pointsG -= 3;
+        System.out.println("Gryffindor gains 30 points");
+        pointsG += 30;
+        System.out.println("Gryffindor gains 100 points");
+        pointsG += 100;
+        System.out.println("Gryffindor gains 60 points");
+        pointsG += 60;
+
+        System.out.println(pointsG);
+
+
+    }
+    
+
+
+}
