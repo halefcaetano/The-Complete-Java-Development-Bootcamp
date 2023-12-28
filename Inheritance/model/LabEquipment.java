4b9f0bf442282bc7f2612b042578ef4cc4185aa8
HalefCaetano <halef_caetano@hotmail.com>
2023-12-28 15:35:03 -0800

    Inheritance and Polymorphism
    Working with Inheritance, Polymorphism, Abstract Classes to build a program that we can use objects of different classes and treat them as objects of a common superclass.

---
+++ Inheritance/model/LabEquipment.java
@@ -0,0 +1,56 @@
+package model;
+
+public abstract class LabEquipment{
+
+    private String manufacturer;
+    private String model;
+    private int year;
+
+    public static final int MIN_YEAR = 1950;
+
+    public LabEquipment(String manufacturer, String model, int year) {
+        setManufacturer(manufacturer);
+        setModel(model);
+        setYear(year);
+    }
+
+    public LabEquipment(LabEquipment source) {
+        setManufacturer(source.manufacturer);
+        setModel(source.model);
+        setYear(source.year);
+    }
+
+    public abstract LabEquipment clone();
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+    public void setManufacturer(String manufacturer) {
+        if (manufacturer == null || manufacturer.isBlank()){
+            throw new IllegalArgumentException("manufacurer cannot be null or blank");
+        }
+        this.manufacturer = manufacturer;
+    }
+    public String getModel() {
+        return model;
+    }
+    public void setModel(String model) {
+        if (model == null || model.isBlank()){
+            throw new IllegalArgumentException("model cannot be null or blank");
+        }
+        this.model = model;
+    }
+    public int getYear() {
+        return year;
+    }
+    public void setYear(int year) {
+        if (year < MIN_YEAR){
+            throw new IllegalArgumentException("year must be greater or equal to the minimum year");
+        }
+        this.year = year;
+    }
+
+    public abstract String performMaintenance();
+
+    
+}
