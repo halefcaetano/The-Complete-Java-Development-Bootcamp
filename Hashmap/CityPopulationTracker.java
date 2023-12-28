ae58f8f4c9c3e76cc1b80a068ae90f8b3a3f12fc
HalefCaetano <halef_caetano@hotmail.com>
2023-12-27 16:52:29 -0800

    Harshmap
    Working with HarshMap. I created a program that I can add city, state and population using HarshMap Always making sure that the user don't make mistakes when the user is inserting the data.

---
+++ Hashmap/CityPopulationTracker.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+
+public class CityPopulationTracker {
+    private HashMap<String, City> cityPopulations;
+
+    public CityPopulationTracker() {
+        cityPopulations = new HashMap<>();
+    }
+
+    public City getCity(String cityName) {
+        return new City(this.cityPopulations.get(cityName));
+    }
+
+    public void setCity(City city) {
+        this.cityPopulations.put(city.getName(), new City(city));
+    }
+
+    public void addCity(City city) {
+        this.cityPopulations.put(city.getName(), new City(city));
+    }
+}
